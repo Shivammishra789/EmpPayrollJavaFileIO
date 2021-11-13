b05ff6785da2899390e50b1ebf0262150628de21
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-13 21:51:34 +0530

    intial commit

---
+++ src/main/java/com/javaio/EmployeePayroll.java
@@ -0,0 +1,23 @@
+package com.javaio;
+
+public class EmployeePayroll {
+
+	private int  id;
+	private String name;
+	private double salary;
+	
+	public EmployeePayroll(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayroll [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/javaio/EmployeePayrollService.java
@@ -0,0 +1,12 @@
+package com.javaio;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+
+	List<EmployeePayroll> employeePayrollData = new ArrayList<EmployeePayroll>();
+
+	
+	}
